673aad82d2ca05b374773324fa26ae7d132737ba
lolitakon <105233192+lolitakon@users.noreply.github.com>
2024-02-08 22:21:06 +0800

    Update ubuntu_init.sh

--- ubuntu_init.sh
+++ ubuntu_init.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #该shell是本人为了方便快速搭建科学上网工具制作
 #本脚本安装软件如下:1.acme.sh以及所需依赖socat 2.nginx 3.warp 4.Luminati（可选）此为brightdata代理池提供的socks代理工具
+#5. 3xui 一个基于xray的web面板
 #无任何商业性质，原则上不允许转载，转载需询问我本人！
 id=`whoami`
 if [ $id != 'root' ]; then
