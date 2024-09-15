e1a9ad00bfff9eab89aa46f274edcf25595bb6d4
lolitakon <105233192+lolitakon@users.noreply.github.com>
2024-09-15 23:51:39 +0800

    Update ubuntu_init.sh

--- ubuntu_init.sh
+++ ubuntu_init.sh
@@ -16,6 +16,10 @@ apt-get install nginx
 curl https://get.acme.sh | sh
 ln /root/.acme.sh/acme.sh /usr/local/bin/acme
 
+# geoip安装
+apt install libnginx-mod-http-geoip2
+apt install geoipupdate
+
 #warp安装
 curl https://pkg.cloudflareclient.com/pubkey.gpg | sudo gpg --yes --dearmor --output /usr/share/keyrings/cloudflare-warp-archive-keyring.gpg
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/cloudflare-warp-archive-keyring.gpg] https://pkg.cloudflareclient.com/ $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/cloudflare-client.list
