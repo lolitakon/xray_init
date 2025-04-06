bddb58fbe97e9abec1c1765e80418dd27373c3cf
lolitakon <105233192+lolitakon@users.noreply.github.com>
2025-04-06 14:41:45 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -54,7 +54,7 @@ fi
 
 # warp安装
 curl -fsSL https://pkg.cloudflareclient.com/pubkey.gpg | sudo gpg --yes --dearmor --output /usr/share/keyrings/cloudflare-warp-archive-keyring.gpg
-echo 'deb [signed-by=/usr/share/keyrings/cloudflare-warp-archive-keyring.gpg] https://pkg.cloudflareclient.com/ $(lsb_release -cs) main' | sudo tee /etc/apt/sources.list.d/cloudflare-client.list
+echo "deb [signed-by=/usr/share/keyrings/cloudflare-warp-archive-keyring.gpg] https://pkg.cloudflareclient.com/ $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/cloudflare-client.list
 apt-get update
 apt-get install cloudflare-warp
 
